631534ff8bb9349cb507adf4dad884b3b299642a
18813157325 <18813157325@163.com>
2023-02-10 17:39:29 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+######################################################
+st.title('CogAgent')
+
+st.markdown('''
+**A KNOWLEDGE-ENHANCED TEXT REPRESENTATION TOOLKIT FOR NATURAL LANGUAGE UNDERSTANDING**
+''')
