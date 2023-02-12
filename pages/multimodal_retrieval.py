06bd0a74bfe73c7e0d5dba7f95b071afb2a0aa47
18813157325 <18813157325@163.com>
2023-02-12 16:05:46 +0800

    Create multimodal_retrieval.py

---
+++ pages/multimodal_retrieval.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+import numpy as np
+# import cv2 # 计算机视觉
+
+# 加载图像的函数
+# def load_an_image(image):
+#     img = Image.open(image)
+#     return img
+
+######################################################
+st.title('CogAgent')
+
+st.markdown('''
+**A KNOWLEDGE-ENHANCED TEXT REPRESENTATION TOOLKIT FOR NATURAL LANGUAGE UNDERSTANDING**
+''')
+
+st.header("Mutimodal Retrieval")
+
+st.warning('''
+**Write Exit to stop**
+''')
+
+choice = st.selectbox('Select an example or input text',
+                      ['','Which city features a green copper statue of a woman holding a torch?'])
+if choice == '':
+    question = st.text_input('Question', 'Input text here or select an example')
+else:
+    question = st.text_input('Question', choice)
+       
+submit = st.button('SUBMIT')
+
+if submit:
+    if question != 'Exit':
+        ## return
+        result = {
+                "pred_text": 'New York City.', 
+                "retrieval_type": 'image', 
+                "retrieval_content": 'The Statue of Liberty, also known as the Statue of Liberty National Monument, is located on Liberty Island, New York, USA', 
+                "retrieval_image": '/data/yangcheng/MMCoQA/data/final_dataset_images/2b79346aa78121aaaf7e0540983d21ea.jpg'  }
+
+        if question == 'Which city features a green copper statue of a woman holding a torch?':
+            st.json(result)
+            
+        #title1 = st.text_input('Question', 'Input more')
+    else:
+        st.info('Thanks')
+        
+
+
+
